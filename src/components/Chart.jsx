ec2acd7ad29e8f1d0d89afd2f177d1553d4b8955
Sophie Krüger <krueger.sophie@googlemail.com>
2023-10-23 22:07:23 +0200

    Implement x, y, z and c scales and bindings

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -5,55 +5,44 @@ export default function Chart({data, width, height}) {
 
     const svgRef = useRef();
     const xMetric = 'pis'
-    const yMetric = 'users'
-    const cDimension = 'device'
+    const yMetric = 'sessions'
+    const zMetric = 'users'
+    const cDimension = 'browser'
+    const maxRadius = 30
 
     useEffect(() => {
       // D3.js code for creating a scatter plot
-        const svg = d3.select(svgRef.current);
+      const svg = d3.select(svgRef.current);
 
-        // Add X axis
+      // Add X axis
+      var x = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d[xMetric])])
+        .range([ 0, width ]);
+                    
+      // Add Y axis
+      var y = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d[yMetric])])
+        .range([ height, 0]);
 
-        // .range([vis.config.margin.top, vis.config.height - vis.config.margin.bottom])
-        // .domain(d3.range(vis.config.rowsN));
-        console.log(data)
+      // Add a scale for bubble size
+      var z = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d[zMetric])])
+        .range([0, maxRadius]);
 
-        var x = d3.scaleLinear()
-          .domain([0, d3.max(data, d => d[xMetric])])
-          .range([ 0, width ]);
-        svg.append("g")
-          .attr("transform", "translate(0," + height + ")")
-          .call(d3.axisBottom(x));
-          
-          // console.log(d3.max(data, d => d[xMetric]))
-           
-        // Add Y axis
-        var y = d3.scaleLinear()
-          .domain([d3.max(data, d => d[yMetric]), 0])
-          .range([ height, 0]);
-        svg.append("g")
-          .call(d3.axisLeft(y));
-      
-        // // Add a scale for bubble size
-        // var z = d3.scaleLinear()
-        //   .domain([200000, 1310000000])
-        //   .range([ 4, 40]);
-      
-        // // Add a scale for bubble color
-        // var myColor = d3.scaleOrdinal()
-        //   .domain(["Asia", "Europe", "Americas", "Africa", "Oceania"])
-        //   .range(d3.schemeSet2);
+      // Add a scale for bubble color
+      var c = d3.scaleOrdinal()
+        .domain(d3.map(data, d => d[cDimension]))
+        .range(d3.schemeSet2);
 
-        svg
-        .selectAll('circle')
-        .data(data)
-        .enter()
-        .append('circle')
-        .attr('cx', (d) => x(d[xMetric]))
-        // .attr('cy', (d) => (d[1]))
-        .attr('cy', (d) => y(d[yMetric]))
-        .attr('r',2 )
-        .attr('fill', 'blue');
+      svg
+      .selectAll('circle')
+      .data(data)
+      .enter()
+      .append('circle')
+      .attr('cx', (d) => x(d[xMetric]))
+      .attr('cy', (d) => y(d[yMetric]))
+      .attr('fill', (d) => c(d[cDimension]))
+      .attr('r', (d) => z(d[zMetric]));
     }, [data, width, height]);
   
     return (
